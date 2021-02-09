303381cfa76015618702655e2dbdfe43a12eedda
yugesh <yugeshkon@gmail.com>
2021-02-09 11:58:20 +0000

    copy same doc

---
+++ src/main/java/com/gamutkart/App.java
@@ -0,0 +1,14 @@
+package com.gamutkart;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+		int i;
+		for(i=0;i<=500;i++)
+		{
+			i += 3;
+        	System.out.println( "print number: " + i);
+   		}
+	 }
+}
